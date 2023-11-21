7c7fd3c4953446f82c7f127bbf56041270465cb8
shamiulhaqq <144481457+shamiulhaqq@users.noreply.github.com>
2023-11-21 10:28:24 +0000

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,25 @@
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace TestLED {
+    /**
+     * TODO: Blink led at specific interval
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+    //% block
+    export function foo(x: number, y: number, interval: number): void {
+        // Add code here
+    }
+
+    /**
+     * TODO: describe your function here
+     * @param value describe value here, eg: 5
+     */
+    //% block
+    export function fib(value: number): number {
+        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    }
+}
